9afe0ef39e1eda1655118cc5edeec428d7ccaf6e
Blaise Hala <blaisehala@gmail.com>
2022-05-02 01:42:37 +0300

    adds the news  sources request logic

--- app/requests.py
+++ app/requests.py
@@ -1,21 +1,72 @@
+from app import app 
 import urllib.request
 import json
-from  .models import articles
+from  .models import articles,source
 
 
 # Articles = articles.Articles
 # Sources = source.Source
 
-#  setting up our request
-# BASE_SOURCE_URL = app.config['NEWS_URL']
-# BASE_ARTICLES_URL = app.config['ARTICLES_URL']
-# api_key = app.config['NEWS_API_KEY']
 
+api_key = None
+BASE_SOURCE_URL = None
+BASE_ARTICLES_URL = None
+
+#  setting up our request
 def configure_request(app):
-    global api_key,BASE_SOURCE_URL,BASE_ARTICLES_URL
-    BASE_SOURCE_URL = app.config['SOURCE_URL']
-    BASE_ARTICLES_URL = app.config['ARTICLES_URL']
-    api_key = app.config['NEWS_API_KEY']
+  global api_key, BASE_ARTICLES_URL, BASE_ARTICLES_URL,BASE_ARTICLES_URL
+BASE_SOURCE_URL = app.config['NEWS_URL']
+BASE_ARTICLES_URL = app.config['ARTICLES_URL']
+api_key = app.config['NEWS_API_KEY']
+
+
+def get_sources():
+  source_url = BASE_SOURCE_URL.format(api_key)
+  #opening the data, reading it and passing it as a dict
+  with urllib.request.urlopen(source_url) as news_source:
+    # Reading the source data
+       source_data = news_source.read()
+       source_data_dict = json.loads(source_data)
+       #  Json.loads converts into a dictionary 
+
+       source_results = None 
+
+       if source_data_dict['sources']:
+           sources_list = source_data_dict['sources']
+           source_results = process_sources(sources_list)
+
+
+           return source_results
+
+
+def process_sources (sources):
+  sources_list = []
+  for source in sources:
+    id = source['id']
+    name = source['name']
+    description = source['description']
+    url = source['url']
+
+    if url:
+      source_object = source(id,name, description,url)
+      sources_list.append(source_object)
+
+
+      return sources_list
+
+
+
+
+
+
+        
+
+
+
+
+
+      
+     
 
 
 
@@ -48,7 +99,7 @@ def process_results(results):
         time = result['publishedAt']
 
         if image:
-            articles_object = Articles(image, description, time)
+            articles_object = articles.Articles(image, description, time)
             articles_results.append(articles_object)
 
     return articles_results
