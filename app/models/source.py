402e4ce1731a8e35350e97ecad4b5125ff1ffc79
Blaise Hala <blaisehala@gmail.com>
2022-05-01 15:35:48 +0300

    defines our news source and news articles models

---
+++ app/models/source.py
@@ -0,0 +1,9 @@
+from base64 import urlsafe_b64decode
+
+
+class Source :
+  def __init__(self,id,name, description,url):
+    self.id= id
+    self.name= name
+    self.description= description 
+    self.url = url
