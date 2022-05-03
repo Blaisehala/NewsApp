1b18081d0bf4b6d027f757d74b26b079e6ec55bc
Blaise Hala <blaisehala@gmail.com>
2022-05-03 15:41:57 +0300

    continous integrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,30 @@
 
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__,instance_relative_config=True)
+# app = Flask(__name__,instance_relative_config=True)
 
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-bootstrap=Bootstrap(app)
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
 
-from app import views
+bootstrap=Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
+
