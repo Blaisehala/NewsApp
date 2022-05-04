9a77be85205333df0ea3a6f6c371c3fdb3b3e4b6
Blaise Hala <blaisehala@gmail.com>
2022-05-04 08:40:31 +0300

    test

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
 from app import create_app
-from flask_script import Manager,Server
+# from flask_script import Manager,Server
 
 
 app=create_app('production')
 
-manager = Manager(app)
-manager.add_command('server',Server)
+# manager = Manager(app)
+# manager.add_command('server',Server)
 
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
 
     
